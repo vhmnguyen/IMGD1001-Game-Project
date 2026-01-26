d851e1ca3ba0ab5b230bdafaf3d294ae5714b771
vnguyen <vhnguyen1@wpi.edu>
2026-01-26 16:46:06 -0500

    Add Version Notes Screen, fix Splash Screen, moved scripts to the correct folder

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -14,7 +14,7 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_version_notes_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/version_notes.tscn")
 
 func _on_credits_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Credits.tscn")
