55892e62c096edf9d40e8877e65e2c0172025a12
ThomasKGilbert <thomaskentgilbert@icloud.com>
2026-02-02 18:59:26 -0500

    Fixed Pugalion size

--- scripts/pugalion.gd
+++ scripts/pugalion.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -225.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
--- scripts/splash_screen.gd
+++ scripts/splash_screen.gd
@@ -3,3 +3,4 @@ extends Node2D
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	get_tree().change_scene_to_file("res://scenes/Main_Menu.tscn")
+	
