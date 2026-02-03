55ec1e43ad0d6b3dcba007787ce60e282712f047
vnguyen <vhnguyen1@wpi.edu>
2026-02-03 17:14:07 -0500

    Initial implementation of Ground Goblin enemy, files refactoring, WPI splash screen loads faster

--- scripts/ground_goblin.gd
+++ scripts/ground_goblin.gd
@@ -1,8 +1,19 @@
-extends CharacterBody2D
+extends Node2D
+
+const SPEED = 80
+
+var direction = -1
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _on_detection_area_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
-func _on_detection_area_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+func _process(delta: float) -> void:
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite_2d.flip_h = true
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite_2d.flip_h = false
+	
+	position.x += direction * SPEED * delta
