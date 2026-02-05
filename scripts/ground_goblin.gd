9124079505118c1a1b1efb6294bfbb772c38a8bb
ThomasKGilbert <thomaskentgilbert@icloud.com>
2026-02-04 19:05:47 -0500

    Player combat and enemy death

--- scripts/ground_goblin.gd
+++ scripts/ground_goblin.gd
@@ -3,17 +3,28 @@ extends Node2D
 const SPEED = 80
 
 var direction = -1
+var dead = false
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
-func _process(delta: float) -> void:
-	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite_2d.flip_h = true
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite_2d.flip_h = false
-	
-	position.x += direction * SPEED * delta
+
+#func _process(delta: float) -> void:
+	#if ray_cast_left.is_colliding():
+		#direction = 1
+		#animated_sprite_2d.flip_h = true
+	#if ray_cast_right.is_colliding():
+		#direction = -1
+		#animated_sprite_2d.flip_h = false
+	#
+	#position.x += direction * SPEED * delta
+
+func _on_hurtbox_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Headbutt"):
+		dead = true
+		animated_sprite_2d.play("death")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "death":
+		queue_free()
--- scripts/pugalion.gd
+++ scripts/pugalion.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -225.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var isAttacking = false
+@onready var collision_shape: CollisionShape2D = $CollisionArea/CollisionShape2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,15 +23,21 @@ func _physics_process(delta: float) -> void:
 	# Flip the sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
+		
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
 	# Play animations
 	if is_on_floor():
-		if direction == 0:
+		if direction == 0 && isAttacking == false:
 			animated_sprite.play("idle")
-		else:
+		elif direction == 0 && isAttacking == true:
+			animated_sprite.play("attack")
+		elif direction != 0 && isAttacking == false:
 			animated_sprite.play("run")
+		else:
+			direction = 0
+			animated_sprite.play("attack")
 	else:
 		animated_sprite.play("jump")
 			
@@ -39,5 +46,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_just_pressed("attack") && is_on_floor():
+		isAttacking = true
+		collision_shape.disabled = false
 
 	move_and_slide()
+
+func _on_animated_sprite_animation_finished() -> void:
+	if animated_sprite.animation == "attack":
+		isAttacking = false
+		collision_shape.disabled = true
