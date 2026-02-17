65ece4c6ff88f3b75bb0a501ceb8ef97233ff023
ThomasKGilbert <thomaskentgilbert@icloud.com>
2026-02-16 19:28:30 -0500

    Adjusted tilemap scale + drew outline of environment + eliminated gravity while dashing in air

--- scripts/pugalion.gd
+++ scripts/pugalion.gd
@@ -28,7 +28,10 @@ func _process(delta: float) -> void:
 func physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if isDashing:
+			velocity.y = 0
+		else:
+			velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
