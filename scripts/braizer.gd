c719fb8290a442121021197b49257fb24a76c3ef
ThomasKGilbert <thomaskentgilbert@icloud.com>
2026-02-28 23:14:45 -0500

    Level Polish

--- scripts/braizer.gd
+++ scripts/braizer.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var next_level_path
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Pugalion:
@@ -9,5 +10,9 @@ func _on_body_entered(body: Node2D) -> void:
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int() + 1
 		
-		var next_level_path = "res://scenes/level_" + str(next_level_number) + ".tscn"
+		next_level_path = "res://scenes/level_" + str(next_level_number) + ".tscn"
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "blaze":
 		get_tree().change_scene_to_file(next_level_path)
